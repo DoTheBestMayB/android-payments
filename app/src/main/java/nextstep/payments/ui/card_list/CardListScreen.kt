55ed9298547e1f355a0f43039f0dd71e25bc6705
DoDoBest <send1684@gmail.com>
2025-08-15 11:00:38 +0900

    feat: 카드 추가 로직 테스트를 위해 카드 목록 화면에 이동 버튼 추가

--- app/src/main/java/nextstep/payments/ui/card_list/CardListScreen.kt
+++ app/src/main/java/nextstep/payments/ui/card_list/CardListScreen.kt
@@ -1,5 +1,10 @@
 package nextstep.payments.ui.card_list
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -9,5 +14,28 @@ fun CardListScreenRoot(
     navigateToNewCard: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    CardListScreen(
+        navigateToNewCard = navigateToNewCard,
+        modifier = modifier,
+    )
+}
 
+@Composable
+fun CardListScreen(
+    navigateToNewCard: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(
+        modifier = modifier,
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding),
+        ) {
+            Button(
+                onClick = navigateToNewCard,
+            ) {
+                Text("카드 추가 화면으로 이동")
+            }
+        }
+    }
 }
