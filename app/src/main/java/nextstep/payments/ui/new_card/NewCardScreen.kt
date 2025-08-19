aaadf3867a6e59a598407ce4c46cbdd0545b0ac4
DoTheBestMayB <send1684@gmail.com>
2025-08-20 00:04:00 +0900

    chore: 사용하지 않는 import 제거 및 코드 정렬

--- app/src/main/java/nextstep/payments/ui/new_card/NewCardScreen.kt
+++ app/src/main/java/nextstep/payments/ui/new_card/NewCardScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -55,9 +54,11 @@ fun NewCardScreenRoot(
             NewCardEvent.CardAddSuccess -> {
                 navigateToCardList()
             }
+
             NewCardEvent.CardAddFail -> {
                 Toast.makeText(context, cardAddFailMessage, Toast.LENGTH_SHORT).show()
             }
+
             NewCardEvent.NavigateBack -> {
                 navigateToCardList()
             }
