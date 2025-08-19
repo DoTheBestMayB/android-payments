99cbc7e0671f1d24a04f46cbc5feb52fb05f0c28
DoTheBestMayB <send1684@gmail.com>
2025-08-19 22:39:42 +0900

    build: targetSdk 36으로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "nextstep.payments"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "nextstep.payments"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
