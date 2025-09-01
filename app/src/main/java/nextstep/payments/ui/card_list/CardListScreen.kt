a84c6612976288758d85c1f22d570a15dc8b7dda
DoTheBestMayB <send1684@gmail.com>
2025-09-01 12:19:08 +0900

    test: 카드 목록 화면 UI 테스트 추가

--- app/src/main/java/nextstep/payments/ui/card_list/CardListScreen.kt
+++ app/src/main/java/nextstep/payments/ui/card_list/CardListScreen.kt
@@ -20,6 +20,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -70,6 +72,9 @@ fun CardListScreen(
                             fontSize = 18.sp,
                             modifier = Modifier
                                 .padding(end = 16.dp)
+                                .semantics {
+                                    contentDescription = "TopAppBar 카드 추가 버튼"
+                                }
                                 .clickable {
                                     navigateToNewCard()
                                 },
@@ -128,6 +133,9 @@ private fun EmptyCardScreen(
             fontWeight = FontWeight.Bold,
             fontSize = 18.sp,
             modifier = Modifier
+                .semantics {
+                    contentDescription = "새로운 카드 등록 안내 문구"
+                }
                 .padding(vertical = 32.dp)
         )
         PaymentCardAdd(
